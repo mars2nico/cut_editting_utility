81704deb9c8b24504ad2ff1333d7008908a639eb
mars2nico <13172812+mars2nico@users.noreply.github.com>
2022-01-28 12:46:26 +0900

    update

--- test/filter/fvad_visualization.sh
+++ test/filter/fvad_visualization.sh
@@ -2,6 +2,15 @@
 
 pushd fvad_visualization
 
-ffmpeg ${1:--f lavfi -i smptehdbars} ${2:--f lavfi -i sine=1000} -to 0:0:15 -filter_complex_script sample.txt -map [out0] -map 1:a -f matroska - | ffplay -i -
+ffmpeg -f lavfi -i ${1:-smptehdbars} -f lavfi -i ${2:-sine=1000} -to 0:0:15 -filter_complex_script sample.txt -map [out0] -map 1:a -f matroska - | ffplay -i -
 
 popd
+
+# Example
+# 1) Exec by test source
+# $ ./fvad_visualization.sh
+#
+# 2) Exec with a file
+# $ ./fvad_visualization.sh movie=`realpath ../data/in.mp4` amovie=`realpath ../data/in.mp4`
+#
+# Note: Absolute path required
